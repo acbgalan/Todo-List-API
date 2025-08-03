91f66455c34b90dd0fdd4151e32ef6a919427031
Carlos Blanco <acbgalan@gmail.com>
2025-08-03 02:41:46 +0200

    Actions to retrieve and create

---
+++ src/TodoList.Server/Controllers/TodosController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Data.Entities;
+using TodoList.Data.Repositories;
+
+namespace TodoList.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodosController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        [HttpGet("{id:int}", Name = "GetTodo")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Todo>> GetTodo(int id)
+        {
+            var todo = await _todoRepository.GetAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound("Todo not found");
+            }
+
+            return Ok(todo);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<Todo>>> GetsTodo()
+        {
+            var todoList = await _todoRepository.GetAllAsync();
+
+            if (!todoList.Any())
+            {
+                return NotFound("Todos not found");
+            }
+
+            return Ok(todoList);
+        }
+
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> CreateTodo([FromBody] Todo todo)
+        {
+            if (todo == null)
+            {
+                return BadRequest();
+            }
+
+            await _todoRepository.AddAsync(todo);
+            int saveResult = await _todoRepository.SaveAsync();
+
+            if (!(saveResult > 0))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "");
+            }
+
+            return CreatedAtRoute("GetTodo", new { id = todo.Id }, todo);
+        }
+
+
+    }
+}

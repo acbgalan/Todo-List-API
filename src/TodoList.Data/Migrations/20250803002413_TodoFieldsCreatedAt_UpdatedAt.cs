da88448e0ceb6f48562ff5c592d984e28ad69b84
Carlos Blanco <acbgalan@gmail.com>
2025-08-03 02:26:31 +0200

    EF migration. New fields to manage the dates of the Todo entity.

---
+++ src/TodoList.Data/Migrations/20250803002413_TodoFieldsCreatedAt_UpdatedAt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TodoFieldsCreatedAt_UpdatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Todos",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Todos",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Todos");
+        }
+    }
+}

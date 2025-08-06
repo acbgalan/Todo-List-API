968e45a8435e13eb214fdca0a529035508cb3d1a
Carlos Blanco <acbgalan@gmail.com>
2025-08-06 02:24:05 +0200

    A DTO is created to manage service responses. A service is created to handle the Todos. Example files are deleted

---
+++ src/TodoList.Server/Services/TodoService/TodoService.cs
@@ -0,0 +1,33 @@
+﻿using TodoList.Shared;
+using TodoList.Shared.Todo;
+
+namespace TodoList.Server.Services.TodoService
+{
+    public class TodoService : ITodoService
+    {
+        public Task<ServiceResult<TodoResponse>> CreateTodoAsync(CreateTodoRequest createTodoRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<bool?>> DeleteTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<TodoResponse>> GetTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<List<TodoResponse>>> GetTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<bool?>> UpdateTodoAsync(UpdateTodoRequest updateTodoRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

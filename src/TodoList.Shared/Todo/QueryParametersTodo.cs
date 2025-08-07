6c874d3a5c3ce86ed2f533b14f6ef8bce719d417
Carlos Blanco <acbgalan@gmail.com>
2025-08-07 15:47:25 +0200

    New DTO for performing searches, filtering, and paginating results

---
+++ src/TodoList.Shared/Todo/QueryParametersTodo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Shared.Todo
+{
+    public class QueryParametersTodo
+    {
+        public string? SearchTerm { get; set; }
+        public string? SortBy { get; set; }
+        public bool SortDesc { get; set; } = false;
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; } = 10;
+    }
+}

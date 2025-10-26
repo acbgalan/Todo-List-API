31dfab367fb8706509b03c089b283254ceb04fd2
Carlos Blanco <acbgalan@gmail.com>
2025-10-26 20:01:55 +0100

    DTOs for login an his response

---
+++ src/TodoList.Shared/User/UserLoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Shared.User
+{
+    public class UserLoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
---
+++ src/TodoList.Shared/User/UserLoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Shared.User
+{
+    public class UserLoginResponse
+    {
+        public required string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}

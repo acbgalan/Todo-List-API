e802208f4f738391fcbcaa95dae0ab0a3d36705d
Carlos Blanco <acbgalan@gmail.com>
2025-10-29 14:25:36 +0100

    Login action and fixes for build token

--- src/TodoList.Server/Controllers/UsersController.cs
+++ src/TodoList.Server/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -16,11 +17,13 @@ namespace TodoList.Server.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly IConfiguration _configuration;
 
-        public UsersController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        public UsersController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
             _configuration = configuration;
         }
 
@@ -38,8 +41,7 @@ namespace TodoList.Server.Controllers
 
             if (result.Succeeded)
             {
-                var registerResponse = await BuildToken<UserRegisterRequest, UserRegisterResponse>(userRegister);
-                return registerResponse;
+                return await BuildToken<UserRegisterRequest, UserRegisterResponse>(userRegister);
             }
             else
             {
@@ -48,6 +50,31 @@ namespace TodoList.Server.Controllers
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
 
+                return ValidationProblem();
+            }
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<UserLoginResponse>> Login(UserLoginRequest userLogin)
+        {
+            var user = await _userManager.FindByEmailAsync(userLogin.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Incorrect login");
+                return ValidationProblem();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user!, userLogin.Password!, false);
+
+            if (result.Succeeded)
+            {
+                return await BuildToken<UserLoginRequest, UserLoginResponse>(userLogin);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Incorrect login");
                 return ValidationProblem();
             }
         }
@@ -70,7 +97,7 @@ namespace TodoList.Server.Controllers
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var expiration = DateTime.UtcNow.AddMonths(1);
 
-            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, signingCredentials: credentials);
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiration, signingCredentials: credentials);
             var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
 
             var response = new TResponse

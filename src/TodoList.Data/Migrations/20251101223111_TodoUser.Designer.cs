373d68bcf92969a0117131e7315883f6a13a1ede
Carlos Blanco <acbgalan@gmail.com>
2025-11-01 23:35:14 +0100

    Entity Framework migration to add user field to table Todo

--- src/TodoList.Data/Migrations/20251101223111_TodoUser.Designer.cs
+++ src/TodoList.Data/Migrations/20251101223111_TodoUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoList.Data.Contexts;
 
@@ -11,9 +12,11 @@ using TodoList.Data.Contexts;
 namespace TodoList.Data.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251101223111_TodoUser")]
+    partial class TodoUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

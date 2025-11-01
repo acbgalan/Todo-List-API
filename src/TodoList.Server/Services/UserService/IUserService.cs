2ae1e0d19faa4a0ee08f1b11727ebd5033059864
Carlos Blanco <acbgalan@gmail.com>
2025-11-02 00:25:28 +0100

    Get the user using the email claim found in HttpContext., It is also registered as a service to make it available via dependency injection.

---
+++ src/TodoList.Server/Services/UserService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TodoList.Server.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<IdentityUser?> GetUser();
+    }
+}

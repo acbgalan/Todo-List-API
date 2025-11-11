3ea29623ef9bec91e5ff5d2e10839e269dde8844
Carlos Blanco <acbgalan@gmail.com>
2025-11-11 19:07:21 +0100

    Validation added and  EF migration applied

--- src/TodoList.Data/Entities/User.cs
+++ src/TodoList.Data/Entities/User.cs
@@ -11,6 +11,7 @@ namespace TodoList.Data.Entities
     public class User : IdentityUser
     {
         [Required]
+        [StringLength(256, ErrorMessage = "The field {0} must contain {1} characters")]
         public required string Name { get; set; }
     }
 }
